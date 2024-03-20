ac915206b05d116443a41cb1c7c3047c4fbd203a
Bernardo A Mira García <bamira1@misena.edu.co>
2024-03-19 21:13:18 -0500

    Otros
    otros

---
+++ MyApp/app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+    Button btnEnviar;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        btnEnviar = findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Editable texto = editText.getText();
+
+                textView.setText(texto);
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
