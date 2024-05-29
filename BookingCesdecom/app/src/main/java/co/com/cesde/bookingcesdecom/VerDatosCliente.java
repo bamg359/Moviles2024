82dd6a14fd3f6b1e86810cb76a08ba5901a14d72
Nayrobiv <bamira1@misena.edu.co>
2024-05-29 11:06:32 -0500

    Clase 29 de mayo
    Mayo

---
+++ BookingCesdecom/app/src/main/java/co/com/cesde/bookingcesdecom/VerDatosCliente.java
@@ -0,0 +1,14 @@
+package co.com.cesde.bookingcesdecom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerDatosCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_datos_cliente);
+    }
+}
